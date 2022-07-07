9f357dfa58fcbe7b3d33dc755b1b85c2aafeff01
Nithin Muthukumar <kathrikai30201094@gmail.com>
2022-07-07 04:19:27 -0400

    fix parent issue add controller system

--- src/blocks.rs
+++ src/blocks.rs
@@ -4,16 +4,16 @@ use rand::prelude::*;
 use super::assetloader::BlockSheet;
 #[derive(Component,Clone)]
 pub struct Tetromino{
-    blocks: [Vec2; 4]
+    index:usize,
+    rot_index:usize
 }
 impl Tetromino{
     fn rand()->Tetromino{
         let mut rng = thread_rng();
         let r = rng.gen_range(0..=6);
-        return Tetromino{blocks:TETROMINOS[r][0]};
+        return Tetromino{index:r,rot_index:0};
     }
 }
-
 pub struct SpawnTetrominoEvent{
     init_pos: Vec3,
 }
@@ -23,18 +23,21 @@ impl Plugin for BlocksPlugin{
         
         app.add_event::<SpawnTetrominoEvent>()
             .add_startup_system(level)
-            .add_system(spawn_tetromino);
+            .add_system(spawn_tetromino)
+            .add_system(tetromino_controller)
+            .add_system(position_blocks)
+            ;
     }
 }
 
 
 
-#[derive(Component,Default)]
-struct Block;
+#[derive(Component)]
+pub struct Block;
 
 pub fn level(mut spawn_event:EventWriter<SpawnTetrominoEvent>){
     spawn_event.send(SpawnTetrominoEvent{
-        init_pos:Vec3::ZERO
+        init_pos:Vec3::new(0.,0.,0.)
     });
 }
 
@@ -42,32 +45,92 @@ pub fn level(mut spawn_event:EventWriter<SpawnTetrominoEvent>){
 pub fn spawn_tetromino(mut cmd: Commands,block_sheet: Res<BlockSheet>, mut event:EventReader<SpawnTetrominoEvent>){
     for ev in event.iter(){
         let tetro = Tetromino::rand();
-        let parent = cmd.spawn()
+        cmd.spawn()
             .insert(tetro.clone())
             .insert(Transform{
                 translation:ev.init_pos,
-                scale: Vec3::new(1.,1.,1.),
                 ..default()
-        }).id();
-        for n in tetro.blocks{
-            let child = cmd.spawn_bundle(SpriteSheetBundle {
-                sprite: TextureAtlasSprite {
-                    index: 7,
+            })
+        .insert(GlobalTransform{
+            translation:ev.init_pos,
+            ..default()
+        })
+        .with_children(|parent| {
+            for n in TETROMINOS[tetro.index][tetro.rot_index] {
+                parent.spawn_bundle(SpriteSheetBundle {
+                    sprite: TextureAtlasSprite {
+                        index: 7,
+                        ..default()
+                },
+                transform: Transform{
+                    translation:n.extend(0.)*16.,
+                    scale:Vec3::new(0.25,0.25,0.25),
                     ..default()
                 },
                 texture_atlas: block_sheet.0.clone(),
                 ..default()
-            }).id();
-            // cmd.entity(parent).push_children(&[child]);
+                }).insert(Block);
+            }
 
 
-            
-        }
-        
+        }); 
+
 
 
     }
 }
+
+pub fn tetromino_controller(
+    mut cmd: Commands,
+    input: Res<Input<KeyCode>>,
+    mut query: Query<(&mut Transform, &mut Tetromino)>){
+    for (mut transform,mut tetro) in query.iter_mut(){
+        if input.just_pressed(KeyCode::W) {
+            tetro.rot_index+=1;
+            tetro.rot_index%=TETROMINOS[tetro.index].len();
+
+        } else if input.just_pressed(KeyCode::S) {
+            if tetro.rot_index==0{
+                tetro.rot_index=TETROMINOS[tetro.index].len()-1;
+            }else{
+                tetro.rot_index-=1;
+            }
+
+
+        }
+        if input.just_pressed(KeyCode::A) {
+            transform.translation.x-=16.;
+
+        } else if input.just_pressed(KeyCode::D) {
+            transform.translation.x+=16.;
+        }
+
+    }
+
+}
+pub fn position_blocks(
+    mut blocks: Query<(&mut Transform),With<Block>>,
+    mut tetrominos:Query<(&mut Tetromino,&Children),Without<Block>>){
+    for (tetro,children) in tetrominos.iter_mut(){
+        for (i,child) in children.iter().enumerate(){
+            if let Ok(mut transform) = blocks.get_mut(*child){
+                
+                
+
+                transform.translation=TETROMINOS[tetro.index][tetro.rot_index][i].extend(0.)*16.;
+            }
+
+            
+
+
+
+        }
+        
+    }
+}
+
+
+
 
 
 
